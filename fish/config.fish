8b84f3b0ffee993d5bab4eb3542faf6d07fe954b
pilladipesh33 <pilladipesh.pd@gmail.com>
2024-01-03 22:00:53 +0530

    dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,67 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ll -al"
+alias lla "ll -A"
+alias g git
+
+#command -qv nvim && alias nvim
+
+set -Ux EZA_STANDARD_OPTIONS --long --all
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+    status --is-command-substitution; and return
+
+    if test -f .nvmrc; and test -r .nvmrc
+        nvm use
+    else
+    end
+end
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
+
+# BREW
+set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
+
+# EZA
+set -U fish_user_paths /path/to/eza $fish_user_paths
+
+#STARSHIPS
+starship init fish | source
