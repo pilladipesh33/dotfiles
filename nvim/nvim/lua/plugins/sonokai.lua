d83f61016bbd3b89602237317040e146f2623e43
pilladipesh33 <pilladipesh.dp@gmail.com>
2024-06-20 22:51:02 +0530

    initial dotfiles

---
+++ nvim/nvim/lua/plugins/sonokai.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      colorscheme = "poimandres",
+    },
+  },
+  -- Lua
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup {
+        require("poimandres").setup {
+          bold_vert_split = false, -- use bold vertical separators
+          dim_nc_background = true, -- dim 'non-current' window backgrounds
+          disable_background = true, -- disable background
+          disable_float_background = false, -- disable background for floats
+          disable_italics = false, -- disable italics
+        },
+      }
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    -- init = function() vim.cmd "colorscheme poimandres" end,
+  },
+}
