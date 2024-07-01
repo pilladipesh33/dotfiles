771455256477394e4d44ae0b48ea906ff261b6a2
pilladipesh33 <pilladipesh.dp@gmail.com>
2024-07-01 23:28:19 +0530

    lsp configuration fix

--- nvim/nvim/lua/plugins/sonokai.lua
+++ nvim/nvim/lua/plugins/sonokai.lua
@@ -27,12 +27,12 @@ return {
   --   -- init = function() vim.cmd "colorscheme poimandres" end,
   -- },
   {
-  "cdmill/neomodern.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("neomodern").setup({
-      -- optional configuration here
+    "cdmill/neomodern.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("neomodern").setup({
+        -- optional configuration here
         style = "roseprime",
         transparent = true,
         term_colors = true,
@@ -44,26 +44,26 @@ return {
         },
 
         -- UI options --
-  ui = {
-    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-    colored_docstrings = true, -- if true, docstrings will be highlighted like strings, otherwise they will be highlighted like comments
-    plain = false, -- don't set background for search
-    show_eob = true, -- show the end-of-buffer tildes
+        ui = {
+          cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+          colored_docstrings = true, -- if true, docstrings will be highlighted like strings, otherwise they will be highlighted like comments
+          plain = false,           -- don't set background for search
+          show_eob = true,         -- show the end-of-buffer tildes
 
-    -- Plugins Related --
-    lualine = {
-      bold = true,
-      plain = false, -- use a less distracting lualine. note: works best when no lualine separators are used
-    },
-    telescope = "borderless", -- choose between 'borderless' or 'bordered'
-    diagnostics = {
-      darker = true, -- darker colors for diagnostic
-      undercurl = true, -- use undercurl for diagnostics
-      background = true, -- use background color for virtual text
-    },
-  },
+          -- Plugins Related --
+          lualine = {
+            bold = true,
+            plain = false,   -- use a less distracting lualine. note: works best when no lualine separators are used
+          },
+          telescope = "bordered", -- choose between 'borderless' or 'bordered'
+          diagnostics = {
+            darker = true,   -- darker colors for diagnostic
+            undercurl = true, -- use undercurl for diagnostics
+            background = true, -- use background color for virtual text
+          },
+        },
       })
-    require("neomodern").load()
-  end,
-},
+      require("neomodern").load()
+    end,
+  },
 }
--- nvim/nvim/lua/plugins/treesitter.lua
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,22 @@
 ---@type LazySpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua",
-        "vim",
-        -- add more arguments for adding more treesitter parsers
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			-- add more things to the ensure_installed table protecting against community packs modifying it
+			opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+				"javascript",
+				"typescript",
+				"css",
+				"gitignore",
+				"graphql",
+				"http",
+				"json",
+				"scss",
+				"sql",
+				"vim",
+				"lua",
+			})
+		end,
+	},
 }
