6cf42cff8c17fcd5a1428fa913b589813b69b9d9
pilladipesh33 <pilladipesh.dp@gmail.com>
2024-07-07 20:40:45 +0530

    nvim config done

--- nvim/nvim/lua/plugins/ui.lua
+++ nvim/nvim/lua/plugins/ui.lua
@@ -69,29 +69,14 @@ return {
   -- buffer line
   {
     "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-        -- separator_style = "slant",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-      },
-    },
+    enabled = false,
   },
   {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function(_, opts)
-      local logo = [[
- ]]
-
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    opts = {
+      indent = { char = "┊" },
+    },
   },
 }
